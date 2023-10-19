ede221b6952064de68ff754bb23e6c52c583ab82
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2023-10-19 17:16:06 +0300

    Inicial Commit

---
+++ Module24-ADO/SkillFactory-Module24/ADO-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using ADO_Dll;
+
+var connector = new MainConnector();
+
+var result = connector.ConnectAsync();
+
+if (result.Result) {
+    Console.WriteLine("Подключено успешно!");
+} 
+else {
+    Console.WriteLine("Ошибка подключения!");
+}
---
+++ Module24-ADO/SkillFactory-Module24/ADO-Dll/MainConnector.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ADO_Dll;
+
+public class MainConnector
+{
+    SqlConnection connection;
+    public async Task < bool > ConnectAsync() {
+        bool result;
+        // SqlConnection connection;
+        try {
+            connection = new SqlConnection(ConnectionString.MsSqlConnection);
+            await connection.OpenAsync();
+            result = true;
+        } 
+        catch {
+            result = false;
+        }
+
+        return result;
+    }
+    
+    // public async void DisconnectAsync() {
+    //     if (connection.State == ConnectionState.Open) {
+    //         await connection.CloseAsync();
+    //     }
+    // }
+}
