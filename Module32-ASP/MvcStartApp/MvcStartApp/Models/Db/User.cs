84513eca040dd50aabfaeccf934f63ff24e6316e
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2024-04-15 15:19:35 +0300

    Delete Module32-ASP directory

--- Module32-ASP/MvcStartApp/MvcStartApp/Models/Db/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MvcStartApp.Models.Db;
-
-public class User
-{
-    public Guid Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public DateTime JoinDate { get; set; }
-}
--- Module32-ASP/MvcStartApp/MvcStartApp/Models/Feedback.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MvcStartApp.Models.Db;
-
-public class Feedback
-{
-    public string From { get; set; }
-    public string Text { get; set; }
-}
